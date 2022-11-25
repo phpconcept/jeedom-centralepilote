fecff0b1d83e95342535c6a7f0289d407eb51212
Vincent <vincent@phpconcept.net>
2022-11-25 16:18:00 +0000

    Separate upgrade function per verion

--- plugin_info/install.php
+++ plugin_info/install.php
@@ -45,8 +45,14 @@ function centralepilote_update() {
   $v_version = config::byKey('version', 'centralepilote', '');
   log::add('centralepilote', 'info', "Update plugin 'centralepilote' from version ".$v_version." to ".CP_VERSION);
   
-  // ----- Fresh install (or verion 0.1)
-  if ($v_version == '') {
+  // ----- Look for specific upgrade from versions
+  if (CP_VERSION == '0.2') {
+    centralepilote_update_v_0_2($v_version);
+  }
+  
+  /*
+  // ----- Update from verion 0.1
+  if ($v_version == '0.1') {
     // ----- Create a default centrale object (if not exists)
     centralepilote::cpCentraleCreateDefault();
     
@@ -108,19 +114,98 @@ function centralepilote_update() {
       }
       
       // ----- Reset commands display
-      $v_eq->cpCmdResetDisplay();
-      
+      $v_eq->cpCmdResetDisplay();      
     }
     
     // ----- Actions for 'centrale'
     else if ($v_eq->cpIsType(array('centrale'))) {
     }  
   }
+  
+  // ----- Update potential list of programm
+  centralepilotelog::cpCmdAllProgrammeSelectUpdate();
+  
+  */
     
   // ----- Save current version
   config::save('version', CP_VERSION, 'centralepilote');
 
   log::add('centralepilote', 'info', "Finished update of plugin 'centralepilote' to ".CP_VERSION);  
+}
+
+
+function centralepilote_update_v_0_2($v_from_version='') {
+    
+  // ----- Update from verion 0.1
+  if ($v_from_version != '0.1') {
+    // TBC : Is downgrade is allowed ???
+    centralepilotelog::log('debug', "Unexpected upgrade from other version than 0.1 ... trying ...");
+  }
+
+  // ----- Create a default centrale object (if not exists)
+  centralepilote::cpCentraleCreateDefault();
+  
+  // ----- Check that at least the default programm is ok
+  centralepilote::cpProgCreateDefault();
+
+  // ----- Global actions at each install (like cleanups)
+  $eqLogics = eqLogic::byType('centralepilote');
+  foreach ($eqLogics as $v_eq) {
+  
+    // ----- Actions for 'radiateur' and 'zone'
+    if ($v_eq->cpIsType(array('radiateur','zone'))) {
+    
+      // ----- Look to rename cmd 'mode' cmd by cmd 'etat' (migration from 0.1 to 0.2+)
+      $v_cmd = $v_eq->getCmd(null, 'mode');
+      if (is_object($v_cmd)) {
+        centralepilotelog::log('debug', "Device '".$v_eq->getName()."' : Remove cmd 'mode' and create cmd 'etat'");
+        $v_cmd->remove();
+        $v_eq->cpCmdCreate('etat', ['name'=>'Etat', 'type'=>'info', 'subtype'=>'string', 'isHistorized'=>1, 'isVisible'=>1, 'order'=>7]);
+      }
+    
+      // ----- Look to remove cmd 'manuel'
+      $v_cmd = $v_eq->getCmd(null, 'manuel');
+      if (is_object($v_cmd)) {
+        centralepilotelog::log('debug', "Device '".$v_eq->getName()."' : Remove cmd 'manuel'");
+        $v_cmd->remove();
+      }
+      // ----- Look to remove cmd 'prog_select'
+      $v_cmd = $v_eq->getCmd(null, 'prog_select');
+      if (is_object($v_cmd)) {
+        centralepilotelog::log('debug', "Device '".$v_eq->getName()."' : Remove cmd 'prog_select'");
+        $v_cmd->remove();
+      }
+      
+      // ----- Look to add cmd
+      $v_cmd = $v_eq->getCmd(null, 'programme_id');
+      if (!is_object($v_cmd)) {
+        centralepilotelog::log('debug', "Device '".$v_eq->getName()."' : Add missing cmd 'programme_id'");
+        $v_eq->cpCmdCreate('programme_id', ['name'=>'Programme Id', 'type'=>'info', 'subtype'=>'string', 'isHistorized'=>0, 'isVisible'=>0, 'order'=>9]);
+      }
+      $v_cmd = $v_eq->getCmd(null, 'programme_select');
+      if (!is_object($v_cmd)) {
+        centralepilotelog::log('debug', "Device '".$v_eq->getName()."' : Add missing cmd 'programme_select'");
+        $v_eq->cpCmdCreate('programme_select', ['name'=>'Programme Select', 'type'=>'action', 'subtype'=>'select', 'isHistorized'=>0, 'isVisible'=>1, 'order'=>10]);
+      }    
+      
+      // ----- Config to add
+      if ($v_eq->getConfiguration('programme_id', '') == '') {
+        centralepilotelog::log('debug', "Device '".$v_eq->getName()."' : Add missing configuration 'programme_id'");
+        $v_eq->setConfiguration('programme_id', '0');
+      }
+      
+      // ----- Reset commands display
+      $v_eq->cpCmdResetDisplay();      
+    }
+    
+    // ----- Actions for 'centrale'
+    else if ($v_eq->cpIsType(array('centrale'))) {
+    }  
+  }
+  
+  // ----- Update potential list of programm
+  centralepilotelog::cpCmdAllProgrammeSelectUpdate();
+    
 }
 
 
